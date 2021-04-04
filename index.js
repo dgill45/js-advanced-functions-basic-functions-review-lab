14a3a3bf1317728919724906cdc32b8df90d0d6c
donellgill <donell.gill@outlook.com>
2021-04-04 13:40:23 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Your code here
+
+saturdayFun();
+
+    function saturdayFun(camp ="roller-skate") {
+        return `This Saturday, I want to ${camp}!`
+}
+    saturdayFun()
+    
+
+let mondayWork = function(study="go to the office"){
+    return `This Monday, I will ${study}.` 
+
+}
+    mondayWork()
+    
+
+function wrapAdjective (flair="*") {
+    return function(angry="special"){
+    return `You are ${flair}${angry}${flair}!`
+ }
+}
+    
+
+let Calculator = {
+    add: function (a,b){
+        return a+b
+    } ,
+    subtract: (a,b) => {
+        return a - b
+    } ,
+    multiply: (a,b) => {
+        return a * b
+    } ,
+    divide: (a,b) => {
+        return a / b
+    } ,
+}
+    
+
+    
+
+    function actionApplyer (start, array){
+    let a = start
+    for(let i = 0; i < array.length; i++){
+    a = array[i](a)
+    }
+    return a
+    }
